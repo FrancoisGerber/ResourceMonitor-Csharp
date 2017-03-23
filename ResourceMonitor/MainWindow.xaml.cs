54983250ba1ce5d4c77f0971937237c52c25928a
francois gerber <francois gerber>
2017-03-23 10:18:28 +0200

    project setup

---
+++ ResourceMonitor/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Management;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace ResourceMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PerformanceCounter diskPerc = new PerformanceCounter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lblName.Content = Environment.MachineName + " - " + Environment.UserName;
+            MyTimer.Start(100);
+
+            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
+            ni.Icon = new System.Drawing.Icon("Main.ico");
+            ni.Visible = true;
+            ni.ContextMenu = new System.Windows.Forms.ContextMenu();
+
+            ni.ContextMenu.MenuItems.Add("Show", (s, e) =>
+            {
+                this.WindowState = WindowState.Normal;
+            });
+            ni.ContextMenu.MenuItems.Add("Hide", (s, e) =>
+            {
+                this.WindowState = WindowState.Minimized;
+            });
+            ni.ContextMenu.MenuItems.Add("Exit", (s, e) => Environment.Exit(0));
+           
+        }
+
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoWork();
+        }
+
+        private void DoWork()
+        {
+            MyTimer.TimeEvent += (() =>
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    try
+                    {
+                        MyTimer.GetDisk(diskPerc);
+                        decimal disk = int.Parse(Decimal.Round(decimal.Parse(MyTimer.Disk.ToString()), 0).ToString());
+
+                        if (disk <= 100)
+                            lblDisk.Content = disk.ToString();
+                        else
+                            lblDisk.Content = "100";
+                        lblCPU.Content = MyTimer.cpu.ToString();
+                        lblRam.Content = int.Parse(Decimal.Round(decimal.Parse(MyTimer.ram), 0).ToString());
+                        pbCpu.Value = MyTimer.cpu;
+                        pbRam.Value = int.Parse(Decimal.Round(decimal.Parse(MyTimer.ram), 0).ToString());
+                        pbDisk.Value = int.Parse(Decimal.Round(decimal.Parse(MyTimer.Disk.ToString()), 0).ToString());
+                        PowerStatus power = SystemInformation.PowerStatus;
+                        int powerPercent = (int)(power.BatteryLifePercent * 100);
+                        if (powerPercent <= 100)
+                        {
+                            pbBat.Value = powerPercent;
+                            lblBattery.Content = powerPercent;
+                        }
+                        else
+                            pbBat.Value = 0;
+                        lblDate.Content = DateTime.Now.Date;
+                        lblTime.Content = DateTime.Now.TimeOfDay.ToString(@"hh\:mm");
+                        lblCharge.Content = power.BatteryChargeStatus.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }));
+            });
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void mClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void mCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+        }
+
+        private void mCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = false;
+        }
+    }
+}
---
+++ ResourceMonitor/MyTimer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResourceMonitor
+{
+    internal static class MyTimer
+    {
+        internal static int cpu;
+        internal static float Disk;
+        internal static string ram;
+        internal static bool IsDone;
+
+        internal delegate void TimeDelegate();
+        internal static event TimeDelegate TimeEvent;
+
+        public static void GetDisk(PerformanceCounter diskPerc)
+        {
+            diskPerc.CategoryName = "PhysicalDisk";
+            diskPerc.CounterName = "% Disk Time";
+            diskPerc.InstanceName = "_Total";
+            diskPerc.MachineName = Environment.MachineName;
+
+            Disk = diskPerc.NextValue();
+                       
+        }
+
+        public static int GetCpuUsage()
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", Environment.MachineName);
+            cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            return (int)cpuCounter.NextValue();
+        }
+        
+        internal static void Start(int Interval)
+        {
+            Task t = Task.Run(() =>
+            {
+                IsDone = false;
+                while (!IsDone)
+                {
+                    cpu = GetCpuUsage();
+                    Int64 phav = PerformanceInfo.GetPhysicalAvailableMemoryInMiB();
+                    Int64 tot = PerformanceInfo.GetTotalMemoryInMiB();
+                    decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
+                    decimal percentOccupied = 100 - percentFree;
+                    // Console.WriteLine("Available Physical Memory (MiB) " + phav.ToString());
+                    //   Console.WriteLine("Total Memory (MiB) " + tot.ToString());
+                    //  Console.WriteLine("Free (%) " + percentFree.ToString());
+                    // Console.WriteLine("Occupied (%) " + percentOccupied.ToString());
+                    // decimal d = Decimal.Parse(percentOccupied);
+
+                    decimal ram2 = Decimal.Round(percentOccupied, 2); 
+                    decimal rounded = Decimal.Round(percentOccupied, 2);
+                    ram = rounded.ToString();
+
+                    if (TimeEvent != null)
+                    {
+                        TimeEvent();
+                    }
+                    Thread.Sleep(Interval);
+                }
+            });
+        }
+    }
+}
